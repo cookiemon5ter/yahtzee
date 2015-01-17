3c71c15fe48dbb547a598ae2c4bfdf1595ede191
Matthew Turner <turner14108@gmail.com>
2015-01-16 21:43:26 -0500

    Added game play, now i just need to refactor and check for bugs.

--- yahtzee.py
+++ yahtzee.py
@@ -13,8 +13,6 @@ class Player:
         for key in self.upper + self.lower:
             self.scores[key] = None
 
-        self.bonus = 0
-
         self.total = 0
 
 
@@ -23,10 +21,51 @@ class Game:
         self.player = Player()
 
     def run(self):
-        for i in range(2):
+        for i in range(13):
             print "roll %d" % (i + 1)
             roll = yaht.getnew()
-            print "%s your roll\n" % roll
+            # print "%s your roll\n" % roll
+
+            while True:
+                print "%s your roll\n" % roll
+                item = raw_input("what would you like to do: ")
+                if item == "show":
+                    self.show_board()
+                    continue
+                elif self.add_score(item, roll):
+                    break
+                else:
+                    continue
+
+            # print self.player.scores # for debugging
+
+    def show_board(self):
+        print "upper score"
+        print "-------------"
+        for i in Player().upper:
+            if self.player.scores[i] is None:
+                pts = 'x'
+            else:
+                pts = self.player.scores[i]
+            print "%-8s = %s" % (i, pts)
+
+        print "\nlower score"
+        print "-------------"
+        for i in Player().lower:
+            if self.player.scores[i] is None:
+                pts = 'x'
+            else:
+                pts = self.player.scores[i]
+            print "%-8s = %s" % (i, pts)
+
+    def sum_score(self):
+        upper = sum([self.player.scores[i] for i in self.player.upper])
+        lower = sum([self.player.scores[i] for i in self.player.lower])
+
+        if upper >= 63:
+            upper += 35
+
+        return upper + lower
 
     def add_score(self, item, dice):
         functions = {"ones": yaht.ones(dice),
